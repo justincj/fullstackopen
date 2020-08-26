53ef4002bfeaf0a5f706b8c6bc720190275f2d1e
justin <justincgeorge95@gmail.com>
2020-08-26 23:48:14 +0530

    part 2.6 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmission = (e) => {
     e.preventDefault();
-    setPersons([...persons, { name: newName }]);
+    for (let person of persons) {
+      if (person.name.toLowerCase() === newName.toLowerCase()) {
+        alert(`${newName} is already added to phonebook`);
+        setNewName("");
+        setNewNumber("");
+        return;
+      }
+    }
+    setPersons([...persons, { name: newName, number: newNumber }]);
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -20,12 +29,21 @@ const App = () => {
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
