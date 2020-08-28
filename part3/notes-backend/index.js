b38f83e2c86396b1dafbdefff10fcf0110de1605
justin <justincgeorge95@gmail.com>
2020-08-28 13:41:27 +0530

    notes

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { response } = require("express");
 
 const app = express();
+app.use(express.json());
 
 let notes = [
   {
@@ -25,6 +27,47 @@ let notes = [
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    notes = notes.filter((n) => n.id !== id);
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+  if (!body.content) {
+    return res.status(400).json({ error: "content missing" });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: String(new Date()),
+    id: generateId(),
+  };
+  notes = notes.concat(note);
+  response.json(note);
 });
 
 const PORT = 3001;
