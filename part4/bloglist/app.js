1e39fe28be7e8bb964815d5208eae400c0b4b4b0
justin <justincgeorge95@gmail.com>
2020-09-02 10:32:17 +0530

    part4 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const blogRouter = require("./controllers/blog");
+
+app.use(cors());
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to", config.MONGODB_URI);
+  })
+  .catch((error) => {
+    console.log("error connecting", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
