8ff9ed4f416538ebec5faa24ecdfc05940f7f04d
justin <justincgeorge95@gmail.com>
2020-09-10 17:23:05 +0530

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,6 +3,18 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const sum = blogs.map((blog) => blog.likes).reduce((acc, val) => acc + val);
+  return sum;
+};
+
+const favouriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes);
+  return blogs.filter((blog) => blog.likes === Math.max(...likes))[0];
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favouriteBlog,
 };
