a195d79e68af3496e15cd19de996fbe60c70853c
justin <justincgeorge95@gmail.com>
2020-09-26 14:09:27 +0530

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showWhenDetails = { display: view ? "" : "none" };
+
+  const toggleDetails = () => {
+    setView(!view);
+  };
+  const incrementLike = () => {};
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleDetails}>{view ? "hide" : "view"}</button>
+      <div style={showWhenDetails}>
+        <p> {blog.url} </p>
+        <div>
+          likes {blog.likes}
+          <button onClick={incrementLike}>like</button>
+        </div>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
