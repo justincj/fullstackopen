9b0851bdf37f3485a47e13687b31a14dd6470797
justin <justincgeorge95@gmail.com>
2020-10-01 21:55:58 +0530

    7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -117,15 +117,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...content} reset="" />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...author} reset="" />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...info} reset="reset" />
         </div>
         <button type="submit">create</button>
         <button type="button" onClick={handleReset}>
